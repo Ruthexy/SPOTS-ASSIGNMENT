439bdc9246d3e407d47fa0da2ba007ef149f2e4c
Ruthexy <ruthokwuokenye2019@gmail.com>
2025-06-07 12:54:29 +0100

    final styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import Profile from "../components/Profile";
 import NewPostForm from "../components/NewPostForm";
-import ImageList from "./ImageList"
+import ImageList from "../ImageList"
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -16,10 +16,10 @@ function Home() {
       <Header />
       <Profile />
       <NewPostForm onAddPost={handleAddPost} />
-      {posts.map((post, index) => (
+      
         <ImageList />
-      ))}
-      <footer>2024 © Spots</footer>
+      
+      
     </div>
   );
 }
